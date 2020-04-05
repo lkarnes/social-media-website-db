2b9ce9503366ecfcfc2d16527f5415bf6626ac42
Logan Karnes <logan.karnes98@gmail.com>
2020-04-04 17:15:10 -0700

    fixed the port problem in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 const server = require('./server.js')
-const port = 6000 || process.env.PORT;
+const port = process.env.PORT || 6000;
 
 server.listen(port, ()=> {console.log(`!!!###Server Listening on ${port}###!!!`)})
