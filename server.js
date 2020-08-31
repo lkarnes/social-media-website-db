d7623a379ae24fa1fbeebf81d745d6349ce7a3fa
Logan Karnes <logan.karnes98@gmail.com>
2020-08-30 19:46:06 -0700

    trial and error

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const postRouter = require('./routers/post-router');
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 server.get('/', (req,res)=> {
     res.status(200).json({status: 'server running and ready for business!'}).catch(err => {
         res.status(500).json(err)
