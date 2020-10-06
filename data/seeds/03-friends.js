db36a5cebefa6164b87cb3009e996e75b8926078
Logan Karnes <logan.karnes98@gmail.com>
2020-10-06 14:02:51 -0700

    converted seeds to friendship_status rather than status

--- data/seeds/03-friends.js
+++ data/seeds/03-friends.js
@@ -5,21 +5,21 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('friends').insert([
-        {user_id: 1, friend_id: 2, status: 'medium'},
-        {user_id: 1, friend_id: 3, status: 'high'},
-        {user_id: 1, friend_id: 4, status: 'low'},
-        {user_id: 1, friend_id: 5, status: 'high'},
-        {user_id: 2, friend_id: 1, status: 'high'},
-        {user_id: 2, friend_id: 3, status: 'medium'},
-        {user_id: 2, friend_id: 4, status: 'high'},
-        {user_id: 2, friend_id: 1, status: 'high'},
-        {user_id: 3, friend_id: 1, status: 'medium'},
-        {user_id: 3, friend_id: 2, status: 'medium'},
-        {user_id: 3, friend_id: 3, status: 'low'},
-        {user_id: 5, friend_id: 2, status: 'medium'},
-        {user_id: 4, friend_id: 3, status: 'low'},
-        {user_id: 4, friend_id: 1, status: 'medium'},
-        {user_id: 4, friend_id: 2, status: 'low'}
+        {user_id: 1, friend_id: 2, friendship_status: 'medium'},
+        {user_id: 1, friend_id: 3, friendship_status: 'high'},
+        {user_id: 1, friend_id: 4, friendship_status: 'low'},
+        {user_id: 1, friend_id: 5, friendship_status: 'high'},
+        {user_id: 2, friend_id: 1, friendship_status: 'high'},
+        {user_id: 2, friend_id: 3, friendship_status: 'medium'},
+        {user_id: 2, friend_id: 4, friendship_status: 'high'},
+        {user_id: 2, friend_id: 1, friendship_status: 'high'},
+        {user_id: 3, friend_id: 1, friendship_status: 'medium'},
+        {user_id: 3, friend_id: 2, friendship_status: 'medium'},
+        {user_id: 3, friend_id: 3, friendship_status: 'low'},
+        {user_id: 5, friend_id: 2, friendship_status: 'medium'},
+        {user_id: 4, friend_id: 3, friendship_status: 'low'},
+        {user_id: 4, friend_id: 1, friendship_status: 'medium'},
+        {user_id: 4, friend_id: 2, friendship_status: 'low'}
       ]);
     });
 };
