b0871a7022d7ecb080dab642943cd0a840008f65
Logan Karnes <logan.karnes98@gmail.com>
2020-10-25 20:48:50 -0700

    added remove comment route

--- routers/comment-router.js
+++ routers/comment-router.js
@@ -12,7 +12,14 @@ router.post('/add', (req,res) => {
     const body = req.body
     console.log('hello')
     comDb.addComment(body).then(response => {
-        res.status(201).json(response)
+        res.status(201).json({message: 'comment added!'})
+    })
+})
+
+router.delete('/remove/:id', (req,res)=> {
+    const id = req.params.id;
+    comDb.removeComment(id).then(response => {
+        res.status(200).json({messsage: 'comment deleted'})
     })
 })
 
